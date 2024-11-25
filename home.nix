4a2dc6d4478df5758e80e41e31b3615cc9d6a48c
awala10 <khalad.awala@gmail.com>
2024-11-25 11:15:40 +0100

    from vim -> neovim

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
     unstable = import <nixos-unstable> {};
     bashsettings = import ./bash.nix;
     packages =  import ./packages.nix pkgs;
+    git = import ./git pkgs;
   in
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -58,14 +59,17 @@
   #  /etc/profiles/per-user/kawala/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    EDITOR = "vim";
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  programs.vim = {
+  programs.neovim= {
     enable = true;
     #package =  unstable.vim;
+    defaultEditor = true;
+    vimAlias = true;
+    viAlias = true;
     plugins = with pkgs.vimPlugins; [
       vim-nftables
       gruvbox-material
@@ -104,64 +108,50 @@
     ];
    # extracConfig = builtins.readFile ./tmux.conf;
    extraConfig = ''
-     set -g default-shell $SHELL
-  #  Mouse
-     set -g mouse on
-
+    set -g default-shell $SHELL
+  # Mouse
+    set -g mouse on
   # History
-  #   set -g history-limit 102400
-
-  #  Set windows and page index to base 1
-  #   set -g base-index 1
-     setw -g pane-base-index 1
-
-   # Re-number windows when creating/closing new windows
-     set -g renumber-windows on
-
+  # set -g history-limit 102400
+  # Set windows and page index to base 1
+  # set -g base-index 1
+    setw -g pane-base-index 1
+  # Re-number windows when creating/closing new windows
+    set -g renumber-windows on
   # Use emacs key bindings in status line
-     set-option -g status-keys emacs
-
+    set-option -g status-keys emacs
   # Use vim key bindings in copy mode
-  #   setw -g mode-keys vi
-
+  # setw -g mode-keys vi
   # Fix ESC delay in vim
-     set -g escape-time 10
-
+    set -g escape-time 10
    # ------------#
    # Keybindings #
    # ------------#
-
    # Set prefix to C-a
-   #  unbind C-b
-  #   set -g prefix C-space
-
-     # Copy-mode
+   # unbind C-b
+  #  set -g prefix C-space
+   # Copy-mode
      unbind-key -T copy-mode-vi v
      bind-key -T copy-mode-vi v send-keys -X begin-selection
      bind-key -T copy-mode-vi 'C-v' send-keys -X rectangle-toggle
      bind-key -T copy-mode-vi y send-keys -X copy-pipe "pbcopy"
      bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "pbcopy"
      bind-key -T copy-mode-vi Escape send-keys -X cancel
-
    # Send command on double press
      bind C-a send-prefix
      bind C-l send-keys 'C-l'
-
-    # Reload tmux.conf on prefix r
-   #  bind r source-file ~/.tmux.conf \; display "Config reloaded!"
-
-  # Split panes and remember current path
+   # Reload tmux.conf on prefix r
+   # bind r source-file ~/.tmux.conf \; display "Config reloaded!"
+   # Split panes and remember current path
      bind '|' split-window -h -c '#{pane_current_path}'
      bind - split-window -v -c '#{pane_current_path}'
-
      # Remember current path when creating new windows
      bind c new-window -c '#{pane_current_path}'
-
      # Break pane into new window and keep focus on the current window
      bind b break-pane -d
      '';
   };
-  programs.git = {
+  programs.git  = {
     enable = true;
     userName = "awala10";
     userEmail = "khalad.awala@gmail.com";
